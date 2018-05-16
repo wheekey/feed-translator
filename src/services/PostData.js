dd158fc4e03a1a00c766ff63a6f41ada9f5c8d32
ermakov@postel-delux.ru <ermakov@postel-delux.ru>
2018-05-16 18:40:31 +0300

    release 0.1

--- src/services/PostData.js
+++ src/services/PostData.js
@@ -1,12 +1,11 @@
 export function PostData(type, userData) {
-    let BaseURL = 'http://feed-translator/api/';
     //let BaseURL = 'http://feed-translator/api/';
+    let BaseURL = 'http://192.168.3.48:83/api/';
 
     return new Promise((resolve, reject) => {
         fetch(BaseURL + type, {
             method: 'POST',
             body: JSON.stringify(userData),
-
         })
             .then((response) => response.json())
             .then((res) => {
